ad106d147a08e2b069bc08a065cb0214de070a2d
Shawn Koski <shawnnkoski@gmail.com>
2018-04-01 13:31:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,7 @@ def batch_badge_creator(guest_list)
   name_badge_array = guest_list.collect{|name| badge_maker(name)}
 end
 
+def assign_rooms(guest_list)
+  room_assignment = []
+  room_assignment = guest_list.each_with_index{|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
